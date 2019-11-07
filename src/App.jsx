bd2726e9e054e3898e8dd8a7c40ee78af6109fee
Jimmy Liao <jimmy.liao@gogoro.com>
2019-11-07 09:41:02 +0800

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import JobSteps from './snapshot/JobSteps';
 import GetServerMessage from './testing-library/GetServerMessage';
 
 function App() {
-  return <div><GetServerMessage /></div>;
+  return (
+    <Router>
+      <div>
+        Please select a component to demo:
+        <ul>
+          <li>
+            <Link to="/job-steps">JobSteps</Link>
+          </li>
+          <li>
+            <Link to="/get-server-message">GetServerMessage</Link>
+          </li>
+        </ul>
+        <hr />
+      </div>
+      <Switch>
+        <Route exact path="/job-steps">
+          <JobSteps step={1} total={3} />
+        </Route>
+        <Route exact path="/get-server-message">
+          <GetServerMessage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
